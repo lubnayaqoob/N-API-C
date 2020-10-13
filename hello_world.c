4562fa64a3e73d1d072f744e79327c4ee5c19f7e
DESKTOP-L5IT98B\Freachly <lubna.yaqoob@freachly.de>
2020-10-13 13:54:52 +0200

    updated read me file

--- hello_world.c
+++ hello_world.c
@@ -1,23 +1,22 @@
-
 #include <node_api.h>
 #include <stdio.h>
-
-napi_value SayHello(napi_env env, napi_callback_info info)
+//hello world native function
+napi_value helloWorld(napi_env env, napi_callback_info info)
 {
-    printf("Hello World Program\n");
+    printf("Hello World Program for my thesis, ----\n");
     return NULL;
 }
 
 napi_value Init(napi_env env, napi_value exports)
 {
     napi_status status;
-
-    napi_value fn;
-    status = napi_create_function(env, NULL, 0, SayHello, NULL, &fn);
+    napi_value result;  //representing the JS function object for the newly created function.& represents a variable address
+    //The native function 'helloWorld' should be called when this function object is invoked.
+  status =   napi_create_function(env, NULL, 0, helloWorld, NULL, &result);
     if (status != napi_ok)
         return NULL;
 
-    status = napi_set_named_property(env, exports, "sayHello", fn);
+    status = napi_set_named_property(env, exports, "helloWorld", result);
     if (status != napi_ok)
         return NULL;
 
--- index.js
+++ index.js
@@ -1,2 +1,3 @@
+//The string passed to require() is the name of the target in binding.gyp responsible for creating the .node file.
 const addon = require('./build/Release/module');
-addon.sayHello();
+addon.helloWorld();
